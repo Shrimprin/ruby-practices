ad81ac4f84bc9ba2c493ea786e385bb9365fd59a
Kuroneko <ken.aster8@gmail.com>
2023-06-24 10:59:44 +0900

    指摘事項３件を修正

--- 02.calendar/cal.rb
+++ 02.calendar/cal.rb
@@ -4,8 +4,7 @@ require 'date'
 
 #-----カレンダーに表示する年月を決める
 #デフォルトは今日の年、月
-year = Date.today.year
-month = Date.today.month
+Time.now => { year:, month:}
 #オプションが有れば上書きする
 opt = OptionParser.new
 opt.banner = "Usage: calender.rb [options]"
@@ -17,9 +16,7 @@ opt.parse!(ARGV)
 print "      \e[35m#{month}\e[0m月 \e[35m#{year}\e[0m\n"
 print "日 月 火 水 木 金 土 \n"
 print '   ' * Date.new(year, month, 1).wday
-last_day = Date.new(year, month, -1).day
-(1..last_day).each do |d|
-  date = Date.new(year,month,d)
-  printf('%2s',(date == Date.today ? "\e[36m#{d}\e[0m" : d))
-  print(Date.new(year,month,d).wday == 6 ? "\n" : ' ')
+(Date.new(year, month, 1)..Date.new(year, month, -1)).each do |d|
+  printf('%2s',(d== Date.today ? "\e[36m#{d.day}\e[0m" : d.day))
+  print(d.saturday? ? "\n" : ' ')
 end
