102f1d9837d5ebad7b2bba6a6a8bbd9db19502c8
Kuroneko <ken.aster8@gmail.com>
2023-12-17 15:39:55 +0900

    マジックナンバーの修正簿れを修正

--- 06.bowling_object/shot.rb
+++ 06.bowling_object/shot.rb
@@ -1,12 +1,16 @@
 # frozen_string_literal: true
 
 class Shot
+  STRIKE_MARK = 'X'
+  STRIKE_SCORE = 10
+
   def initialize(mark)
     @mark = mark
   end
 
   def score
-    return 10 if @mark == 'X'
+    @mark == STRIKE_MARK ? 10 : @mark.to_i
+    return 10 if @mark == STRIKE_MARK
 
     @mark.to_i
   end
